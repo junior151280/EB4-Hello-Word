8687d69c1209237e0c18d6319a6dd85c802ee4d6
Jailton Santos Sales Junior <juniorjailton@hotmail.com>
2019-10-04 21:49:03 -0300

    Carga inicial dos projetos

---
+++ 2.2/DotNetCore-Hello-World-App/src/Calculate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCore_Hello_World_App
+{
+    public class Calculate
+    {
+        
+
+        public int Number1 { get; set; }
+        public int Number2 { get; set; }
+
+        public int Add(int value1, int value2)
+        {
+            return value1 + value2;
+        }
+    }
+}
---
+++ 2.2/DotNetCore-Hello-World-App/tests/UnitTest1.cs
@@ -0,0 +1,36 @@
+using NUnit.Framework;
+using DotNetCore_Hello_World_App;
+
+
+namespace DotNetCore_Hello_World_App_Test
+{
+    public class Tests
+    {
+        Calculate calc = new Calculate();
+
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void PassingTest()
+        {
+            
+            Assert.AreEqual(4, calc.Add(2, 2));
+        }
+
+        [Test]
+        public void FailingTest()
+        {
+            Assert.AreEqual(5, calc.Add(2, 2));
+        }
+    }
+}
