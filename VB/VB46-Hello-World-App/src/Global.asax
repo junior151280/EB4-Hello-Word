bfb7690b263ec89a3da769ae455bdbcccb00b0c1
Jailton Santos Sales Junior <juniorjailton@hotmail.com>
2019-10-20 18:43:19 -0300

    Criando projeto VB4.6

---
+++ VB/VB46-Hello-World-App/src/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="VB46_Hello_World_App.MvcApplication" Language="VB" %>
---
+++ VB/VB46-Hello-World-App/test/My Project/AssemblyInfo.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("VB46-Hello-World-App-Test")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("VB46-Hello-World-App-Test")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+<Assembly: Guid("872cac50-a2df-4efe-8680-a6710ccd9d8c")>
+
+' <Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
