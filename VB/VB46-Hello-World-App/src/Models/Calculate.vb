3952a5c8d13470a9d30e5cb242f0fb02259d465f
Jailton Santos Sales Junior <juniorjailton@hotmail.com>
2019-10-20 19:09:48 -0300

    Implementando teste de unitário

---
+++ VB/VB46-Hello-World-App/src/Models/Calculate.vb
@@ -0,0 +1,6 @@
+﻿Public Class Calculate
+
+    Public Function Add(value1 As Integer, value2 As Integer) As Integer
+        Return value1 + value2
+    End Function
+End Class
--- VB/VB46-Hello-World-App/test/UnitTest1.vb
+++ VB/VB46-Hello-World-App/test/UnitTest1.vb
@@ -1,9 +1,22 @@
 ﻿Imports System.Text
 Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports VB46_Hello_World_App
 
 <TestClass()> Public Class UnitTest1
+    Private _calculate As Calculate = New Calculate
 
-    <TestMethod()> Public Sub TestMethod1()
+    <TestMethod()> Public Sub PassingTest()
+
+        Assert.AreEqual(4, _calculate.Add(2, 2))
+    End Sub
+
+    <TestMethod()> Public Sub FailingTest()
+
+        Assert.AreNotEqual(5, _calculate.Add(2, 2))
+    End Sub
+
+    <TestMethod()> Public Sub Test1()
+
     End Sub
 
 End Class
