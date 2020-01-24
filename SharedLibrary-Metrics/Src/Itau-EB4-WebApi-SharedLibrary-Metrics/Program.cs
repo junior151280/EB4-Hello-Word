11578c850b4592439b43d09648113f2d9f251252
Jailton Santos Sales Junior <juniorjailton@hotmail.com>
2020-01-24 06:54:23 -0300

    Alter framework 3.1 to 2.1

--- SharedLibrary-Metrics/Src/Itau-EB4-WebApi-SharedLibrary-Metrics/Program.cs
+++ SharedLibrary-Metrics/Src/Itau-EB4-WebApi-SharedLibrary-Metrics/Program.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Logging;
 
 namespace Itau_EB4_WebApi_SharedLibrary_Metrics
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
--- SharedLibrary-Metrics/Src/Itau-EB4-WebApi-SharedLibrary-Metrics/Startup.cs
+++ SharedLibrary-Metrics/Src/Itau-EB4-WebApi-SharedLibrary-Metrics/Startup.cs
@@ -1,16 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
 using Itau_EB4_WebApi_SharedLibrary_Metrics.Models;
 
@@ -27,15 +20,14 @@ namespace Itau_EB4_WebApi_SharedLibrary_Metrics
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllers();
+        {            
             services.AddDbContext<PipelineMetricsContext>(options => 
                 options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public static void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
