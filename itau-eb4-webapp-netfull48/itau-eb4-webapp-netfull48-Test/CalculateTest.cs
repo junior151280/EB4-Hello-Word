a6411982c97b715dd4fffd709a88646f85e1a54e
Jailton Santos Sales Junior <juniorjailton@hotmail.com>
2020-08-05 18:08:03 -0300

    Add new project .net4.8

---
+++ itau-eb4-webapp-netfull48/itau-eb4-webapp-netfull48-Test/CalculateTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using itau_eb4_webapp_netfull48.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace itau_eb4_webapp_netfull48_Test
+{
+    [TestClass]
+    public class CalculateTest
+    {
+        private readonly Calculate calculate = new Calculate();
+        [TestMethod]
+        public void PassingTest()
+        {
+            Assert.AreEqual(4, calculate.Add(2, 2));
+        }
+
+        [TestMethod]
+        public void FailingTest()
+        {
+            Assert.AreNotEqual(5, calculate.Add(2, 2));
+        }
+    }
+}
---
+++ itau-eb4-webapp-netfull48/itau-eb4-webapp-netfull48-Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("itau-eb4-webapp-netfull48-Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("itau-eb4-webapp-netfull48-Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e7f53a75-dd52-4266-a5d3-6178601ba206")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
